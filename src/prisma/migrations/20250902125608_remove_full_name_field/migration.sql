81409aea5fb87eada488b8dc876cb68ee1757bcf
Valentyn Kovalskyi <kovalsky.valentine@gmail.com>
2025-09-02 15:56:44 +0300

    Remove field in Language table

---
+++ src/prisma/migrations/20250902125608_remove_full_name_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `full_name` on the `Language` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Language" DROP COLUMN "full_name";
